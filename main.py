683dffbdff601f67e7f2b5e5a6fd9bb1125d5aa3
Madison-Skogsberg-2023 <816418@mystma.org>
2021-10-04 09:34:28 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 #WRITE YOUR CODE IN THIS FILE
+#create function
+#add parameters
+
+def hasL(w):
+
+    for i in range (0, len(w)):
+
+        if w[i] == "l":
+            return True
+    return False
+
+print(hasL("dog"))
